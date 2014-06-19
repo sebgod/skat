2f16d57141053b546f56131a86d29a456b57fb98
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-06-20 00:57:37 +0200

    build.cmd: now uses more generic algorithm to detect mmc

--- build.cmd
+++ build.cmd
@@ -1,3 +1,25 @@
 @pushd %~dp0
-@make MMC=mercury %*
+@setlocal enabledelayedexpansion
+@if defined MERCURY_HOME (
+   call :SET_HOME MMC
+) else (
+   call :FIND_IN_PATH mercury.bat MMC
+)
+
+@if defined MMC goto :MAKE
+@echo Cannot find Mercury compiler executable, MERCURY_HOME=%MERCURY_HOME%
+@exit /b 1
+
+:MAKE
+@make MMC=%MMC% %*
+@set MAKE_RESULT=%ERRORLEVEL%
 @popd
+@echo result=%MAKE_RESULT%
+@exit /b %MAKE_RESULT%
+
+:SET_HOME
+    @endlocal && ( set %1="%MERCURY_HOME%\bin\mmc" ) && exit /b 0
+
+:FIND_IN_PATH
+    @set RESULT=%~dp$PATH:1
+    @endlocal && ( set %2=%RESULT%mmc )
