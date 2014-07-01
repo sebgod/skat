7466083fb8f8180e826e9119f4c248e503ffd2e6
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-07-01 19:52:24 +0200

    eval.m: creating stub for counting high cards

--- eval.m
+++ eval.m
@@ -46,6 +46,8 @@
 
 :- implementation.
 
+:- import_module require.
+:- import_module sparse_bitset.
 :- import_module skat.card.
 :- import_module skat.deck.
 :- import_module skat.rank.
@@ -53,21 +55,34 @@
 
 %----------------------------------------------------------------------------%
 
+:- type suit_set == sparse_bitset(suit).
+
+:- func all_suits = suit_set.
+
+all_suits = list_to_set([diamonds, hearts, spades, clubs]).
+
 evaluate_for_bidding(Player) = eval(Stats) :-
     Deck = Player^player_cards,
     Jacks = Deck `straight_by_rank` jack,
-    Highs =
+    NonTrumps = [jack, ace, ten, king, queen, nine, eight, seven],
     ( Jacks = 4 ->
-        deck_empty
+        foldl2(count_succ(Deck), NonTrumps, deck_empty, Highs, all_suits, _)
     ; Jacks = -4 ->
-        deck_empty
+        Highs = deck_empty
     ;
-        cards_by_rank(Deck, jack)
+        % cards_by_rank(Deck, jack)
+        unexpected($file, $pred, "not yet implemented, Jacks != 4")
     ),
     Stats = stats(Jacks,
                   Deck^deck_suits,
                   Deck^deck_suit_values,
                   Highs^deck_suits).
+
+:- pred count_succ(deck::in, rank::in, deck::in, deck::out,
+    suit_set::in, suit_set::out) is det.
+
+count_succ(Deck, Rank, !Straight, !Suits) :-
+    ByRank = Deck `straight_by_rank` Rank.
 
 %----------------------------------------------------------------------------%
 :- end_module skat.eval.
